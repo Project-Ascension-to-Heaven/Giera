7d41556241691d743d145b53f8a9d4950866d163
kubo0 <72550826+kubo0@users.noreply.github.com>
2020-11-11 17:19:28 +0100

    Alpha 0.0001 (Początki interfejsu)
    Kolumny, header (png), głowna mapa (png), itd.

---
+++ sandbox/Interface.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img id="obrazTlo" src="header.png">
+        <!--<div id="profil">
+            <img src="22.png">
+        </div>
+        <div id="poziom">
+            <img src="22.png">
+        </div>
+        <div id="coins">
+            <img src="22.png">
+        </div>
+        <div id="drewno">
+            <img src="22.png">
+        </div>
+        <div id="surowce">
+            <img src="22.png">
+        </div>
+        <div id="gems">
+            <img src="22.png">
+        </div>!-->
+    </header>
+        <div id="lewo">
+            <img id="obrazTlo" src="projekt.png">
+        </div>
+        <div id="prawo">
+            chuj
+            <div id="prawo2">
+                jebać
+            </div>
+        </div>
+</body>
+</html>
