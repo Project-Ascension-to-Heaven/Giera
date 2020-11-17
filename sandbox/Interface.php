64b7a518940fa53ef4d845044058df3182466918
Andrzej Kebab <grzesiugora123@wp.pl>
2020-11-17 17:12:41 +0100

    no ogólnie to tak
    troche tego i tam tego

--- sandbox/Interface.php
+++ sandbox/Interface.php
@@ -7,37 +7,48 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <header style="background-color: cadetblue;">
-        
-        <div id="profil">
-            
+<body style="background-color: #3d3d3d;">
+        <div class="container-fluid">
+            <div class="row" id="top">
+                <div class="col-2">
+                    Nick: Marek 
+                </div>
+                <div class="col-2">
+                    Gildia: brak
+                </div>
+                <div class="col-1">
+                    Jedzenie: 25125
+                </div>
+                <div class="col-1">
+                    <?php include('time.php')
+                    ?>
+                </div>
+                <div class="col-1">
+                    Metale: 2516 
+                </div>
+                <div class="col-1">
+                    Monety: 61261
+                </div>
+                <div class="col-1">
+                    Kryształy: 21
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-10" id="lewo">
+                    <img src="projekt.png" id="obrazTlo">
+                </div>
+                <div class="col-2">
+                    Siedziba = Maksymalny lvl budynków
+                    Szklarnie = Jedzenie
+                    Tartak = Drewno
+                    Kopalnia = Metale
+                    Mieszkania = Monety
+                    Odwierty = Kryształy
+                </div>
+            </div>
         </div>
-        <div id="poziom">
-            
-        </div>
-        <div id="coins">
-            
-        </div>
-        <div id="drewno">
-            
-        </div>
-        <div id="surowce">
-            
-        </div>
-        <div id="gems">
-            
-        </div>
-    </header>
-        <div id="lewo">
-            <img id="obrazTlo" src="projekt.png">
-        </div>
-        <div id="prawo">
-            chuj
-        </div>
-        <div id="prawo2">
-            jebać
-        </div>
-        
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
 </html>
