74640b64e5fc99892373f6d06d05f92ef3a94ecd
Snaksiu <kewin_mejer@wp.pl>
2020-11-19 13:37:03 +0100

    Surowce i Budynki

--- sandbox/Villageclass.php
+++ sandbox/Villageclass.php
@@ -2,14 +2,78 @@
 class Village 
 {
     private $buildings; 
+    private $storage;
+    private $upgradeCost;
 
     public function __construct()
     {
         $this->buildings = array(
-            'townHall' -> 1,
-            'tartak' -> 1,
+            'townHall' => 1,
+            'tartak' => 1,
+            'kopalniaMetali' => 1,
         );
+        $this->storage = array(
+            'drewno' => 0,
+            'metale' => 0,
+        );
+        $this->upgradeBuilding = array(
+            'tartak' => array(
+                2 => array(
+                    'drewno' => 100,
+                    'metale' => 50,
+                ),
+                3 => array(
+                    'drewno' => 100,
+                    'metale' => 50,
+                ),
+            ),
+            'kopalniaMetali' => array(
+                2 => array(
+                        'drewno' => 100,
+                ),
+                    3 => array(
+                        'drewno' => 300,
+                        'metale' => 150,
+                ),
+            
+            )
+
+         );
+    }
+    public function drewnoGain(int $deltaTime) : float
+    {
+        $gain = pow($this->buildings['tartak'],2) * 10000;
+        $perSecondGain = $gain / 3600;
+        return $perSecondGain * $deltaTime;
+
+    }
+    public function metaleGain(int $deltaTime) : float
+    {
+        $gain = pow($this->buildings['kopalniaMetali'],2) * 5000;
+        $perSecondGain = $gain / 3600;
+        return $perSecondGain * $deltaTime;
+
+    }
+    public function gain($deltaTime)
+    {
+        $this->storage['drewno'] += $this->drewnoGain($deltaTime);
+        $this->storage['metale'] += $this->metaleGain($deltaTime);
+    }
+    public function upgradeBuilding(string $buildingName) : bool
+    {
+        $currentLVL = $this->buildings[$buildingName];
+        $cost = $this->upgradeCost[$buildingName][$currentLVL+1];
+        foreach ($cost as $key => $value) {
+            if($value > $this->storage[$key])
+            return false;
+        }
+        foreach ($cost as $key => $value){
+           $this->storage[$key] -= $value;
+        }
+        $this->buildings[$buildingName] += 1; 
+        return true;
     }
 }
+
 
 ?>
--- sandbox/index.php
+++ sandbox/index.php
@@ -11,22 +11,56 @@
         require('./Villageclass.php');
         session_start();
 
-
+    
         if(!isset($_SESSION['v']))
         {
-            echo "Tworzę nową wioskę"
+            echo "Tworzę nową wioskę";
             $v = new Village();
             $_SESSION['v'] = $v;
+            $deltaTime = time();
         }
         else 
         {
-        $_SESSION['v'] - $v;
+            $_SESSION['v'] - $v;
+            $deltaTime = time() - $_SESSION['time'];
+        }
+        $v->gain($deltaTime);
+
+        if(isset($_REQUEST['action']))
+        {
+            switch($_REQUEST['action'])
+            {
+                case "upgradeBuilding":
+                    if($v->upgradeBuilding($_REQUEST['building']))
+                    {
+                        echo "Ulepszono budynek: ".$_REQUEST['building'];
+                    }
+                    else
+                    {
+                        echo "Kowal spalił ci Truta +8";
+                    }
+                break;
+                default:
+                    echo 'Nieprawidłowa zmienna action';
+
+            }
+
         }
 
-        var_dump($v)
+
+
+
+
+
+        $_SESSION['time'] = time();
+        var_dump($v);
+        var_dump($_REQUEST);
 
 
     ?>
     </pre>
+    <a href=""
+    <button>Rozbuduj Tartak</button>
+    <button>Rozbuduj Kopalnie</button>
 </body>
 </html>
