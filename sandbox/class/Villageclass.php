22a82dccc9a032d3c174b88acf92c1610ce60bdd
Andrzej Kebab <grzesiugora123@wp.pl>
2020-12-07 16:15:12 +0100

    surowce i interfejs

--- sandbox/class/Villageclass.php
+++ sandbox/class/Villageclass.php
@@ -11,10 +11,14 @@ class Village
             'townHall' => 1,
             'tartak' => 1,
             'kopalniaMetali' => 1,
+            'farmy' => 1,
+            'skarbowka' => 0,
         );
         $this->storage = array(
             'drewno' => 0,
             'metale' => 0,
+            'jedzenie' => 0,
+            'monety' => 0,
         );
         $this->upgradeBuilding = array(
             'tartak' => array(
@@ -25,20 +29,45 @@ class Village
                 3 => array(
                     'drewno' => 100,
                     'metale' => 50,
+                    'monety' => 80
                 ),
             ),
             'kopalniaMetali' => array(
                 2 => array(
-                        'drewno' => 100,
+                    'drewno' => 100,
                 ),
-                    3 => array(
-                        'drewno' => 300,
-                        'metale' => 150,
+                3 => array(
+                     'drewno' => 300,
+                     'metale' => 150,
+                     'monety' => 100
                 ),
             
-            )
+            ),
+            'farmy' => array(
+                2 => array(
+                    'drewno' => 50,
+                    'metale' => 100,
+                ),
+                3 => array(
+                    'drewno' => 100,
+                    'metale' => 200,
+                    'monety' => 200,
+                ),
+            ),
+            'skarbówka' => array(
+                1 => array(
+                    'drewno' => 200,
+                    'metale' => 75,
+                    'jedzenie' => 100,
+                ),
+                2 => array(
+                    'drewno' => 400,
+                    'metale' => 140,
+                    'jedznie' => 120,
+                ),
+            ),
 
-         );
+        );
     }
     public function drewnoGain(int $deltaTime) : float
     {
@@ -54,10 +83,26 @@ class Village
         return $perSecondGain * $deltaTime;
 
     }
+    public function jedzenieGain(int $deltaTime) : float
+    {
+        $gain = pow($this->buildings['army'],2) * 3500;
+        $perSecondGain = $gain / 3600;
+        return $perSecondGain * $deltaTime;
+
+    }
+    public function monetyGain(int $deltaTime) : float
+    {
+        $gain = pow($this->buildings['skarbowka'],2) * 1000;
+        $perSecondGain = $gain / 3600;
+        return $perSecondGain * $deltaTime;
+
+    }
     public function gain($deltaTime)
     {
         $this->storage['drewno'] += $this->drewnoGain($deltaTime);
         $this->storage['metale'] += $this->metaleGain($deltaTime);
+        $this->storage['jedznie'] += $this->jedzenieGain($deltaTime);
+        $this->storage['monety'] += $this->monetyGain($deltaTime);
     }
     public function upgradeBuilding(string $buildingName) : bool
     {
@@ -81,6 +126,12 @@ class Village
         break;
             case 'metale';
                 return $this->metaleGain(3600);
+        break;
+             case 'jedznie';
+                return $this->jedzenieGain(3600);
+        break;
+            case 'monety';
+                return $this->monetyGain(3600);
         break;
         default:
             echo "Nie ma takiego surowca!";
