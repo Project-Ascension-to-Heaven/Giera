17208e4be57643d9c6e819fe39e3e52161764e29
kubo0 <72550826+kubo0@users.noreply.github.com>
2021-04-17 19:42:39 +0200

    Smarty... Jezusie święty, miej nad nami opiekę

---
+++ sandbox/smarty/templates_c/db3723d998980077bbf5410c1dbab2f073a0fc5d_0.file.footer.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-04-17 18:57:39
+  from 'D:\xampp\htdocs\Giera\sandbox\smarty\templates\footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_607b1383818c41_40775231',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'db3723d998980077bbf5410c1dbab2f073a0fc5d' => 
+    array (
+      0 => 'D:\\xampp\\htdocs\\Giera\\sandbox\\smarty\\templates\\footer.tpl',
+      1 => 1618678633,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_607b1383818c41_40775231 (Smarty_Internal_Template $_smarty_tpl) {
+?>    <?php echo '<script'; ?>
+ src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+</body>
+</html><?php }
+}
