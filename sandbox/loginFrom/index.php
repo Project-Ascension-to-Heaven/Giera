e7a5e286dc8488c785dfc05bd362609a717729ed
XXAssDestroyerXX <72551615+XXAssDestroyerXX@users.noreply.github.com>
2021-04-18 15:57:45 +0200

    Revert "Dziala?"
    This reverts commit 331f02dbed3f7e0c909058b1f8872c1ecc2c225e.

--- sandbox/loginFrom/index.php
+++ sandbox/loginFrom/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-</head> <!--<sprawdzam czy dziala lolllz>-->
+</head>
 <body>
     <?php
     if(!isset($_REQUEST['username']))
